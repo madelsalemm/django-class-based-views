f5201ac089c57ab36e2e4336f49eda7199066300
madelsalemm <Mohamed.adel.salemm@gmail.com>
2022-10-31 21:23:52 +0200

    add static and media in settings and url

--- post/models.py
+++ post/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length = 50)
     content = models.TextField(max_length = 1000)
-    created_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='post-img/' )
     
-    
-    def __str__(self):
-        pass
-
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
         
+    def __str__(self):
+        return self.title
+
+
     def get_absolute_url(self):
         return reverse('Post_detail', kwargs={'pk': self.pk})
